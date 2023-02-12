c9d952734169b67208b8d3c7805df35dde58cdc7
nagrzesikowska <94862333+nagrzesikowska@users.noreply.github.com>
2023-02-12 12:36:01 +0100

    Search Engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import logo from "./she.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          Coded by Natalia Grzesikowska and {""}
+          <a
+            href="https://github.com/nagrzesikowska/dictionary-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
