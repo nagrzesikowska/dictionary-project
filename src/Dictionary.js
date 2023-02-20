18cf02600bbb011d47d2e31d2b09c8fef6442662
nagrzesikowska <94862333+nagrzesikowska@users.noreply.github.com>
2023-02-20 18:53:22 +0100

    Photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionResponse);
+
+    let pexelsApiKey =
+      "jbqq48f2nmL4X9jRYRQ4GVvXH87XxU21RBOkbNYXJdwORbagwV4tOKXQ";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -35,7 +47,7 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section>
-          <h1>Imput the word below</h1>
+          <h1>What word do you want to look up?</h1>
           <form onSubmit={handleSubmit}>
             <input
               type="search"
@@ -45,6 +57,7 @@ export default function Dictionary(props) {
           </form>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Phonetic.css";
+
 export default function Phonetic(props) {
-  console.log(props.phonetic);
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} rel="noopener">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
       <span className="text">{props.phonetic.text}</span>
